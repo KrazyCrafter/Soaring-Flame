f46fc4d111c75292661a50cde1fb757b75649273
KrazyCrafter <68299410+KrazyCrafter@users.noreply.github.com>
2025-02-22 13:36:03 -0500

    Menu Added
    Added Main Menu Prototype, and can swap to the game scene from there. Also has a credits screen but it is currently empty

---
+++ Soaring Flame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject CreditsMenu;
+    public GameObject ActiveMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActiveMenu = mainMenu;
+        CreditsMenu.SetActive(false);
+        ActiveMenu.SetActive(true);
+    }
+    public void SeeCredits()
+    {
+        ActiveMenu.SetActive(false);
+        ActiveMenu = CreditsMenu;
+        ActiveMenu.SetActive(true);
+    }
+    public void BackToMain()
+    {
+        ActiveMenu.SetActive(false);
+        ActiveMenu = mainMenu;
+        ActiveMenu.SetActive(true);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
