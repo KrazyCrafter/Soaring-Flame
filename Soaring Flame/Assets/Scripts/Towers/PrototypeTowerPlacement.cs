4fed3c08db6b2200fecbd321f969cab229a165c0
KrazyCrafter <68299410+KrazyCrafter@users.noreply.github.com>
2025-04-01 00:00:37 -0400

    Prototype tower placement
    Got a prototype version of tower placement, though I would still prefer if we redid it

---
+++ Soaring Flame/Assets/Scripts/Towers/PrototypeTowerPlacement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrototypeTowerPlacement : MonoBehaviour
+{
+    public GameObject RapiBlaster;
+    public Transform SelectTile;
+    public Vector3 PlacePos;
+    public Quaternion PlaceTurn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 250.0f))
+            {
+                SelectTile = hit.transform;
+                PlacePos = SelectTile.position;
+                PlaceTurn = SelectTile.rotation;
+                Debug.Log("You selected " + SelectTile.tag + " At " + SelectTile.position.x + ", " + SelectTile.position.y + ", " + SelectTile.position.z + ", "); //check what you clicked
+                if (SelectTile.tag == "Placable")
+                {
+                    Instantiate(RapiBlaster, PlacePos, PlaceTurn);
+                    Destroy(SelectTile.gameObject);
+                }
+            }
+        }
+    }
+}
